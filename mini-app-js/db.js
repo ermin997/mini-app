883ecee7fafeb9821fb3e1fe148932c7844ed8b6
ermin997 <73242555+ermin997@users.noreply.github.com>
2021-07-16 14:36:23 +0200

    Add files via upload

---
+++ mini-app-js/db.js
@@ -0,0 +1,18 @@
+let db = [
+    {
+        name : "Danilo",
+        lastName : "Vesovic",
+        email : "danilo@gmail.com",
+        phone : "111-222-333"
+    },
+    {
+        name : "Ivan",
+        lastName : "Ivanovic",
+        email : "ivan@gmail.com",
+        phone : "333-222-111"
+    },
+];
+
+if(localStorage.db){
+    db = JSON.parse(localStorage.db);
+}
